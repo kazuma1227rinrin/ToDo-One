851a7b572f902f51649a95a6209667c5ef81dc08
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-01-11 22:24:36 +0900

    ToDo-Oneにプッシュし直し

---
+++ todo-frontend-file/pages/index.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Todos from '@/components/Todos';
+import CreateTodoForm from '@/components/CreateTodoForm';
+
+const inter = Inter({ subsets: ['latin'] })
+
+// トップページ
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <CreateTodoForm />
+      <Todos />
+    </div>
+  );
+}
